441e114d6754439320a142bbb03d6e4dda961478
Gyeongmin Ha <gaonrudal@gmail.com>
2021-01-09 02:10:57 +0900

    Solve dongbin-na-part3-dfs,bfs-3

--- books/dongbin-na/part3_dfs,bfs/(fail)dfs,bfs_3.py
+++ books/dongbin-na/part3_dfs,bfs/(fail)dfs,bfs_3.py
@@ -1,9 +1,7 @@
 '''
-개선 방안 :heap을 써서 삽입 삭제 logN이다
-포인트: 그냥 맨처음에 순서 낮은 순서로 넣으면 순서 유지된다.
-
-지난 시도 때 틀린 이유:
-처음 Queue 초기화시 virus 체크 안하고 모두 큐에 넣어버림..
+BOJ 18405에서 시간 초과남...
+--> 해결함: 어이없는 실수함.. 
+처음 q 초기화시 0도 큐에 넣어버림. if문 안써서..
 
 3 3
 1 0 2
@@ -19,8 +17,7 @@
 1 2 2
 >>> 0
 '''
-from collections import deque
-import sys
+import heapq, sys
 
 input = sys.stdin.readline
 
@@ -39,29 +36,23 @@ distance = [[0] * (n+1) for _ in range(n+1)] # 시간 S는 0으로 초기화
 dx = [-1, +1, 0, 0]
 dy = [0, 0, -1, +1]
 
-
 # print(n, k)
 # print(data)
 # print(S, X, Y)
 # print(distance)
 
 def bfs(data: list) -> None:
-    q = deque()
+    q = []
     # 초기 바이러스 위치로 큐 초기화
-    tmp = []
     for i in range(1, n+1):
         for j in range(1, n+1):
             if data[i][j] != 0: # is virus
-                tmp.append((data[i][j], i, j)) # (virus, x,)
-    tmp.sort() # asc order, NlogN
-    for t in tmp:
-        q.append(t) # 작은 virus# 순서로 넣음.
-    
+                heapq.heappush(q, (0, data[i][j], i, j)) # dist, virus, x, y
     # bfs
     while q:
-        virus, x, y = q.popleft()
+        dist, virus, x, y = heapq.heappop(q)
 
-        if distance[x][y] == S: # S초뒤 결과가 나왔을 경우 break
+        if dist == S: # S초뒤 결과가 나왔을 경우 break
             break
 
         for i in range(4):
@@ -70,8 +61,8 @@ def bfs(data: list) -> None:
             if 1 <= nx <= n and 1 <= ny <= n: # in map
                 if data[nx][ny] == 0: # empty
                     data[nx][ny] = virus
-                    distance[nx][ny] = distance[x][y] + 1
-                    q.append((virus, nx, ny))
+                    distance[nx][ny] = dist + 1
+                    heapq.heappush(q, (dist + 1, virus, nx, ny))
 bfs(data)
 
 # print(data)
